27ea955ee066524e0bc0afb371e5311dea97bfa3
BenjaMolina <molinasb.94@gmail.com>
2019-03-20 23:55:23 -0600

    Updates

---
+++ precache-manifest.024214848f9097e9881ba4dd5fa550df.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "63452b53289c42ea8b14453a09023f27",
+    "url": "/reactjs_heroes/static/media/daredevil.63452b53.png"
+  },
+  {
+    "revision": "ff564756cb5ab2287505",
+    "url": "/reactjs_heroes/static/css/main.63687781.chunk.css"
+  },
+  {
+    "revision": "7faddb69b03a97694701",
+    "url": "/reactjs_heroes/static/js/1.7faddb69.chunk.js"
+  },
+  {
+    "revision": "c131a7f33a6e29031503",
+    "url": "/reactjs_heroes/static/js/runtime~main.c131a7f3.js"
+  },
+  {
+    "revision": "3d130107d211ef1e37782abfcd0ac2f4",
+    "url": "/reactjs_heroes/static/media/logo.3d130107.ico"
+  },
+  {
+    "revision": "0a5b0047960bb20e0c6154f9090edd3d",
+    "url": "/reactjs_heroes/static/media/aquaman.0a5b0047.png"
+  },
+  {
+    "revision": "2a044fe165a3be74ce7eb3a3f0723b85",
+    "url": "/reactjs_heroes/static/media/batman.2a044fe1.png"
+  },
+  {
+    "revision": "ff564756cb5ab2287505",
+    "url": "/reactjs_heroes/static/js/main.ff564756.chunk.js"
+  },
+  {
+    "revision": "1ff033409a950668c9ff207b0b571372",
+    "url": "/reactjs_heroes/static/media/hulk.1ff03340.png"
+  },
+  {
+    "revision": "79754598c96a5a47de36ff440af138df",
+    "url": "/reactjs_heroes/static/media/linterna-verde.79754598.png"
+  },
+  {
+    "revision": "f341f4c125b137e38ad98c0ede1cf265",
+    "url": "/reactjs_heroes/static/media/spiderman.f341f4c1.png"
+  },
+  {
+    "revision": "9f020bd48589d0a0754b9f4e4320b6c7",
+    "url": "/reactjs_heroes/static/media/wolverine.9f020bd4.png"
+  },
+  {
+    "revision": "76df5dcddea6c0ba874cc47ce92e7f73",
+    "url": "/reactjs_heroes/static/media/DC.76df5dcd.png"
+  },
+  {
+    "revision": "e7b85ddd717cf3d1ba9bbcd722991da6",
+    "url": "/reactjs_heroes/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.427e2ef8fb665406c4dbaec732d28778.js"
+  "/reactjs_heroes/precache-manifest.024214848f9097e9881ba4dd5fa550df.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/reactjs_heroes/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
